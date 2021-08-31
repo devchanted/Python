7b4045da9593a84d72c2ccc03ff62ae90c5b5a17
devchanted <73908739+devchanted@users.noreply.github.com>
2021-08-30 22:50:15 -0700

    Print Statements

---
+++ basics/print.py
@@ -0,0 +1,11 @@
+#print numbers and text
+print(2021 ,"Work From Home")
+
+print("########################")
+print("printing multiple Statements in same line....")
+print(2021, end="")
+print("Work From Home")
+""" This 
+is a
+multiline
+comment"""
