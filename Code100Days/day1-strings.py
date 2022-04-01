67e115d2bbd5ee81bfbaa8446452cfd000212a03
devchanted <73908739+devchanted@users.noreply.github.com>
2022-04-01 12:37:10 -0700

    Day 01 - Strings

---
+++ Code100Days/day1-strings.py
@@ -0,0 +1,25 @@
+# 1.1 - printing excercises
+
+print("Day 1 - Python Print Function")
+print('the function is declared like this:')
+print("print('what to print')")
+
+# 1.2 - Input Functions
+
+city = input("What is you city name?\n")
+pet = input("Whats you pet name?\n")
+print("Your cool nick name is " + city + " " + pet)
+
+# 1.3 - Variables 
+
+# input two variables a and b and print in inverse order
+
+a = input("Enter variable a: ")
+b = input("Enter variable b: ")
+print("Reversing")
+
+c=b
+b=a
+a=c
+print(a)
+print(b)
