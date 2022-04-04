1f30cd40d113093669720c101add9b27588c6565
devchanted <73908739+devchanted@users.noreply.github.com>
2022-04-03 22:01:27 -0700

    basic if else structure

---
+++ Code100Days/day03/if-else.py
@@ -0,0 +1,7 @@
+print("Welcome to Rollercoaster")
+
+height = int(input("Enter your height in cm: "))
+if height > 120:
+    print("you can ride the rollercoaster")
+else:
+    print("sorry. Not tall enough")
