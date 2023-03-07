57175b786613f180a67f43ba80dd5e00100c29c0
devchanted <73908739+devchanted@users.noreply.github.com>
2023-03-06 23:31:36 -0800

    guess the letter in a word

---
+++ Code100Days/day07/guessWord.py
@@ -0,0 +1,15 @@
+import random
+
+word_list = ["apple", "ball", "cat"]
+
+random_word = random.choice(word_list)
+print(random_word)
+
+word_guess = input("Guess the letter: ")
+
+for letter in range(0, len(random_word)):
+    if word_guess == random_word[letter]:
+        print("True")
+    else:
+        print("False")
+
