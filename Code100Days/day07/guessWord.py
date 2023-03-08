2ec8c02d23c8badb32296aafc0870923f2403eb2
devchanted <73908739+devchanted@users.noreply.github.com>
2023-03-07 20:53:30 -0800

    fixing uppercase input

--- Code100Days/day07/guessWord.py
+++ Code100Days/day07/guessWord.py
@@ -5,7 +5,7 @@ word_list = ["apple", "ball", "cat"]
 random_word = random.choice(word_list)
 print(random_word)
 
-word_guess = input("Guess the letter: ")
+word_guess = input("Guess the letter: ").lower()
 
 for letter in range(0, len(random_word)):
     if word_guess == random_word[letter]:
