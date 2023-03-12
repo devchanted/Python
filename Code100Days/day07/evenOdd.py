0c12c4045c59ac2adbf8059171be5e585674b331
devchanted <73908739+devchanted@users.noreply.github.com>
2023-03-11 23:11:18 -0800

    even and odd with if else

---
+++ Code100Days/day07/evenOdd.py
@@ -0,0 +1,31 @@
+""" 
+Given an integer, n , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of 2 to 5, print Not Weird
+If  is even and in the inclusive range of 6 to 20, print Weird
+If  is even and greater than 20 , print Not Weird """
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    if n % 2 != 0:
+        print("Weird")
+    else:
+        if  2<=n<=5:
+            print("Not Weird")
+        elif 6<=n<=20:
+            print("Weird")
+        else:
+            print("Not Weird")
