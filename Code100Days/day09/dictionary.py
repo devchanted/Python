8e2dfcb312e3c72222fb78ba43d2ce871db01f0a
devchanted <73908739+devchanted@users.noreply.github.com>
2023-08-16 23:04:03 -0700

    Adding elements to dictionary

--- Code100Days/day09/dictionary.py
+++ Code100Days/day09/dictionary.py
@@ -7,3 +7,13 @@ baby_first_words = {
 }
 
 print(baby_first_words["A"])
+
+#add items to existing dictioanry
+
+baby_first_words["D"]= "dog"
+
+#print ALL ITEM IN DICTIONARY
+
+for items in baby_first_words:
+    print(items)
+    print(baby_first_words[items])
