747354d98475aa4f2d164b73af1275b3ded769ee
devchanted <73908739+devchanted@users.noreply.github.com>
2024-07-01 23:15:47 -0700

    Nesting List in Dictionary

---
+++ Code100Days/day09/nestingListInDictionary.py
@@ -0,0 +1,10 @@
+# Nesting Dictionary in a Dictionary
+
+
+travel_log = {
+    "USA" : ["San Francisco", "Las Vegas", "Los Angeles"],
+    "UAE" : ["Dubai", "Qatar", "Abudhabi"]
+}
+
+print(travel_log)
+print(travel_log["USA"])
