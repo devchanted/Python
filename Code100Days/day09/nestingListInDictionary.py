b06dc084fca2317cacb092d587cd9b46f4bf8bef
devchanted <73908739+devchanted@users.noreply.github.com>
2024-11-01 22:39:35 -0700

    Nested Dictionary

--- Code100Days/day09/nestingListInDictionary.py
+++ Code100Days/day09/nestingListInDictionary.py
@@ -8,3 +8,27 @@ travel_log = {
 
 print(travel_log)
 print(travel_log["USA"])
+
+#nested list
+
+nested_list = ["A", "B", ["C", "D"]]
+
+print(nested_list[2][1])
+
+#Nested Dictionary 
+
+travel_blog = {
+
+    "USA" : {
+        "total_visits": 4,
+        "cities_visited": ["San Francisco", "Las Vegas", "Los Angeles"]
+    },
+
+    "UAE" : {
+        
+        "cities_visited": ["Dubai", "Qatar", "Abudhabi"],
+        "total_visits" : 1
+    },
+}
+
+print(travel_blog["UAE"]["cities_visited"][2])
