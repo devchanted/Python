3b4e982cf70d98d33293345efe0c060992976443
devchanted <73908739+devchanted@users.noreply.github.com>
2024-11-09 23:13:50 -0800

    memory logic added

--- Code100Days/day10/calculatorWithMemory.py
+++ Code100Days/day10/calculatorWithMemory.py
@@ -11,9 +11,6 @@ def mul(n1,n2):
 def div(n1,n2):
     return n1/n2
 
-x = int(input("Enter Numner 1: "))
-
-
 operation = {
     "+" : add,
     "-" : sub,
@@ -21,9 +18,24 @@ operation = {
     "/" : div
 
 }
+def logic():
+    x = int(input("Enter Numner 1: "))
+    selector = input("Enter the Operations [+, - , *, / ]:  ")
+    y = int(input("Enter Number 2: "))
+    answer = operation[selector](x,y)
+    print(f"{x} {selector} {y} = {answer}")
+    return answer
+answer = logic()
+choice = input(f" Type 'y' to continue calculating with {answer}, or type 'n' to start new calculation: " )
 
-y = int(input("Enter Number 2: "))
-selector = input("Enter the Operations [+, - , *, / ]:  ")
+if choice == "y":
+    selector = input("Enter the Operations [+, - , *, / ]:  ")
+    z = int(input("Enter Number 2: "))
+    new_answer = operation[selector](answer,z)
+    print(f"{answer} {selector} {z} = {new_answer}")
+elif choice == "n":
+    logic()
 
-print(operation[selector](x,y))
+else:
+    print("Error")
 
