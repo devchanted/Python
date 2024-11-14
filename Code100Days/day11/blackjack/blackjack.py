14fe30c01be8fd5f4c703e909c3de1b2abbf23de
devchanted <73908739+devchanted@users.noreply.github.com>
2024-11-13 22:11:02 -0800

    deal card functionality

---
+++ Code100Days/day11/blackjack/blackjack.py
@@ -0,0 +1,23 @@
+import random
+from art import logo
+
+# create a deal card function
+
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+# create a play game and assign two cards to user and computer
+
+def play_game():
+    user_cards = []
+    computer_cards = []
+
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
+    print(user_cards)
+    print(computer_cards)
+
+play_game()
