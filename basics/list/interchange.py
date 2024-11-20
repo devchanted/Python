f76ef4ab6972faa60651d885f18edb4e51aa06e5
devchanted <73908739+devchanted@users.noreply.github.com>
2024-11-19 21:13:13 -0800

    interchange elements in list

---
+++ basics/list/interchange.py
@@ -0,0 +1,16 @@
+#Python program to interchange first and last elements in a list
+
+def swapList(list):
+
+    size = len(list)
+
+    temp = list[0]
+    list[0] = list[size-1]
+    list[size-1] = temp
+    return list
+
+
+
+list = [ 1, 2, 3, 4, 5]
+
+print(swapList(list))
