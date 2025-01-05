e9b596d3fd930e85f2a0818362a02a7241a85df3
devchanted <73908739+devchanted@users.noreply.github.com>
2025-01-04 21:19:09 -0800

    adding one more slicing example

--- basics/strings/sliceString.py
+++ basics/strings/sliceString.py
@@ -3,12 +3,16 @@ url1 = "https://www.example.com"
 url2 = "ftp://ftp.example.org"
 url3 = "http://api.example.net"
 
+filename = "data_2022_01_15.csv"
  # Extract the protocol using slicing by character
 
 protocol1 = url1[:url1.index(":")]
 protocol2 = url2[:url2.index(":")]
 protocol3 = url2[:url2.index(":")]
 
+file_ext = filename[filename.index(".")+1:]
+
 print( "Protocol 1: " , protocol1)
 print( "Protocol 2: " , protocol2)
 print( "Protocol 3: " , protocol3)
+print("File Extention Type: ", file_ext)
